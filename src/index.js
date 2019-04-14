f748352b6dfd1f3eef50b5d61c3d61fd9fdd2b37
dinglu <dinglu.ding@bytedance.com>
2019-04-14 11:26:33 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,14 +3,51 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App/App';
 import Toggle from './Toggle/Toggle';
+import IsLogin from './IsLogin/IsLogin';
+import Page from './Page/Page';
 
 import * as serviceWorker from './serviceWorker';
- 
+
+function Blog(props) {
+  const sidebar = (
+    <ul>
+      {props.posts.map((post) =>
+        <li key={post.id}>
+          {post.title}
+        </li>
+      )}
+    </ul>
+  )
+  const content = props.posts.map((post) => (
+    <div key={post.id}>
+      <h3>{post.title}</h3>
+      <p>{post.content}</p>
+    </div>
+  ))
+
+  return (
+    <div>
+      {sidebar}
+      <hr />
+      {content}
+    </div>
+  );
+}
+
+const posts = [
+  {id:1,title: 'Hello World', content: 'Welcome to learning React!'},
+  {id:2,title: 'Installation', content: 'You can install React from npm!'}
+]
+
+
+
 ReactDOM.render(
-  <Toggle />,
+  // <Toggle />,
+  // <IsLogin isLoggedIn={false} />,
+  // <Page />,
+  <Blog posts={posts} />,
   document.getElementById('root')
 );
-
 
 // setInterval(tick, 1000);
 
